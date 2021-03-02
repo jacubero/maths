0a0f037e4847b8386b9901da0e19e334b16813da
jacubero <jaacubero@gmail.com>
2021-03-02 19:47:15 +0100

    New version

--- Competitions/OME/Nacional/d.sh
+++
@@ -1,4 +0,0 @@
-for i in {93..99}; do
-	wget http://www.olimpiadamatematica.es/platea.pntic.mec.es/_csanchez/sol${i}.zip
-	unzip sol${i}.zip
-done
--- Competitions/OME/Nacional/download.sh
+++
@@ -1,4 +0,0 @@
-for i in {2008..2020}; do
-	wget -O $i.pdf http://www.olimpiadamatematica.es/platea.pntic.mec.es/_csanchez/olimp${i}_archivos/fase_nacional_problemas/enunciados.pdf
-	wget -O ${i}s.pdf http://www.olimpiadamatematica.es/platea.pntic.mec.es/_csanchez/olimp${i}_archivos/fase_nacional_problemas/soluciones.pdf
-done
